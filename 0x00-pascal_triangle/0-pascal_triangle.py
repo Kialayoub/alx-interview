ba3f5d488c53e322bf8dc9d0d74d8702e9b97b70
Kial Ayoub <132392508+Kialayoub@users.noreply.github.com>
2024-06-19 15:33:13 +0100

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,18 +7,16 @@
 def pascal_triangle(n):
     """
     Returns a list of integers
-    representing the Pascal Triangle of n
-    returns empty list if n <= 0
     """
     tab = []
     if n <= 0:
         return tab
     tab = [[1]]
     for i in range(1, n):
-        temp = [1]
+        t = [1]
         for j in range(len(tab[i - 1]) - 1):
             curr = tab[i - 1]
-            temp.append(tab[i - 1][j] + tab[i - 1][j + 1])
-        temp.append(1)
-        tab.append(temp)
+            t.append(tab[i - 1][j] + tab[i - 1][j + 1])
+        t.append(1)
+        tab.append(t)
     return tab
